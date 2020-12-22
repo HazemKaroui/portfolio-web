fa966ee562904b3294bc790cdb4ea1ab0547995c
Hazem Karoui <acertainhazem@gmail.com>
2020-12-22 13:42:54 +0100

    smoother theme transitions

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,15 @@
 let button = document.getElementById("dark-mode-button");
 let root = document.querySelector(':root');
+let stylesheet = document.styleSheets[1];
 let themeIsDark = false;
 
-button.addEventListener("click", switchTheme);
-function switchTheme() {
+button.addEventListener("click", setTheme);
+function setTheme() {
     if (themeIsDark == false) {
         root.style.setProperty('--text-color', 'rgb(255, 255, 255)');
         root.style.setProperty('--accent-color', 'rgb(251, 160, 17)');
         root.style.setProperty('--background-color', 'rgb(20, 33, 61)');
+        stylesheet.cssRules[22].style.listStyle = 'url(/check-square-dark.svg)';
         themeIsDark = true;
     }
 
@@ -15,6 +17,7 @@ function switchTheme() {
         root.style.setProperty('--text-color', 'rgb(14, 21, 21)');
         root.style.setProperty('--accent-color', 'rgb(124, 124, 125)');
         root.style.setProperty('--background-color', 'rgb(236, 238, 240)');
+        stylesheet.cssRules[22].style.listStyle = 'url(/check-square.svg)';
         themeIsDark = false;
     }
 }

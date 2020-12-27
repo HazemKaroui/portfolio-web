0664c1b87695b92527f22495c0a9c24399567a9d
Hazem Karoui <acertainhazem@gmail.com>
2020-12-27 21:19:54 +0100

    new desktop & medium format layout

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("dark-mode-button");
 const root = document.querySelector(':root');
-const stylesheet = document.styleSheets[1];
+const stylesheet = document.styleSheets[0];
 
 //On first visit
 if (!localStorage.getItem('theme'))
@@ -22,13 +22,15 @@ function setTheme() {
         root.style.setProperty('--text-color', 'rgb(255, 255, 255)');
         root.style.setProperty('--accent-color', 'rgb(251, 160, 17)');
         root.style.setProperty('--background-color', 'rgb(20, 33, 61)');
-        stylesheet.cssRules[23].style.listStyle = 'url(images/check-square-dark.svg';
+        root.style.setProperty('--menu-color', 'rgb(28, 47, 87)');
+        stylesheet.cssRules[stylesheet.cssRules.length-1].style.listStyle = 'url(images/check-square-dark.svg';
     }
 
     else {
         root.style.setProperty('--text-color', 'rgb(14, 21, 21)');
         root.style.setProperty('--accent-color', 'rgb(124, 124, 125)');
         root.style.setProperty('--background-color', 'rgb(236, 238, 240)');
-        stylesheet.cssRules[23].style.listStyle = 'url(images/check-square.svg)';
+        root.style.setProperty('--menu-color', 'rgb(245, 245, 245)');
+        stylesheet.cssRules[stylesheet.cssRules.length-1].style.listStyle = 'url(images/check-square.svg)';
     }
 }
--- navigation.js
+++ navigation.js
@@ -1,10 +1,13 @@
 let sections = document.getElementsByClassName('section');
+let links = document.getElementsByClassName('link');
 
 function highlight(sectionId) {
     //make all the sections invisible
     for (let i = 0; i < sections.length; i++) {
         sections.item(i).style.display = 'none';
+        /* links.item(i).style.filter = 'blur(0.15px)'; */
     }
     //display the highlighted section
     document.getElementById(sectionId).style.display = 'flex';
+    /* document.getElementById('link' + sectionId).style.filter = ''; */
 }
